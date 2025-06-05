24d465539da8e67dbe0ac82663df28be2b1f924e
nicoxeye <nicoxeye@op.pl>
2025-06-05 20:38:09 +0200

    whoopsie?

--- src/additional_ga.c
+++ src/additional_ga.c
@@ -2,28 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-#define SIZE 9
-#define BOX 3
-#define SIZE 9
-#define BOX 3
-
-#define POPULATION_SIZE 100
-#define NUM_GENERATIONS 200
-#define MUTATION_RATE 0.05
-#define CROSSOVER_RATE 0.8
-
-// candidate structure
-typedef struct {
-    int grid[SIZE][SIZE];
-    int fitness;
-} Individual;
-
-
-Individual rouletteSelection(Individual population[]);
-void multiPointCrossover(Individual parent1, Individual parent2, Individual *child1, Individual *child2, bool fixed[SIZE][SIZE]);
-void uniformCrossover(Individual parent1, Individual parent2, Individual *child, bool fixed[SIZE][SIZE]);
-void simpleMutate(Individual *individual, bool fixed[SIZE][SIZE]);
+#include "additional_ga.h"
 
 /*
 roulette selection chooses individuals proportionally to their fitness
--- src/additional_ga.h
+++ src/additional_ga.h
@@ -4,5 +4,21 @@
 #define SIZE 9
 #define BOX 3
 
+#define POPULATION_SIZE 100
+#define NUM_GENERATIONS 200
+#define MUTATION_RATE 0.05
+#define CROSSOVER_RATE 0.8
+
+// candidate structure
+typedef struct {
+    int grid[SIZE][SIZE];
+    int fitness;
+} Individual;
+
+
+Individual rouletteSelection(Individual population[]);
+void multiPointCrossover(Individual parent1, Individual parent2, Individual *child1, Individual *child2, bool fixed[SIZE][SIZE]);
+void uniformCrossover(Individual parent1, Individual parent2, Individual *child, bool fixed[SIZE][SIZE]);
+void simpleMutate(Individual *individual, bool fixed[SIZE][SIZE]);
 
 #endif
